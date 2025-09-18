dc38a6323420846f63b77e428d7e1bc0f7706722
Richard Thornton <richt9662@gmail.com>
2025-09-18 15:06:31 +0100

    chore: added some styling to the pages

--- src/components/LARGEIMAGE.jsx
+++ src/components/LARGEIMAGE.jsx
@@ -1,16 +1,21 @@
 //large image goes here//
+import "./LARGEIMAGE.css";
 export default function LARGEIMAGE({ image }) {
   return (
     <>
       {image && (
-        <div className="mb-4 p-4 border-2 border-gray-300 rounded-lg text-center">
-          <h2 className="text-xl font-semibold mb-2">{image.title}</h2>
-          <img
-            src={image.url}
-            alt={image.alt}
-            className="max-w-full h-auto mx-auto mb-2"
-          />
-          <p className="text-gray-600">{image.alt}</p>
+        <div className="large-image-container">
+          <div className="mb-4 p-4 border-2 border-gray-300 rounded-lg text-center">
+            <h2 className="text-xl font-semibold mb-2 text-black">
+              {image.title}
+            </h2>
+            <img
+              src={image.url}
+              alt={image.alt}
+              className="max-w-full h-auto mx-auto mb-2"
+            />
+            <p className="text-black">{image.alt}</p>
+          </div>
         </div>
       )}
     </>
--- src/components/THUMBNAIL.jsx
+++ src/components/THUMBNAIL.jsx
@@ -1,16 +1,5 @@
+import "./THUMBNAIL.css";
 export default function THUMBNAIL({ images, index, handleSwitchImage }) {
-  //state to store api data "in the image component"
-  //state to store index value (to nevigate between images)
-
-  //effects
-  //fetch data from api and update the data state to store the fetched data
-
-  //function to control what index we are clicking on
-  //the index represents a specific image object in the array
-
-  //function handleSwitchImage(index){
-  //setIndex(index)}
-
   return (
     <>
       <section className="thumbnail-section">
